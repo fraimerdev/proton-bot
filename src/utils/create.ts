f4e656a65485b5c1f5ee9b3914d2554dea0a8b0f
Fraimer De La Cruz <fraimerdelacruzjaziel@gmail.com>
2025-12-10 14:23:35 -0400

    Update prisma config

--- src/utils/create.ts
+++ src/utils/create.ts
@@ -1,10 +1,13 @@
 import type { AnyCommand } from "../types/command";
 import type { ClientEventKey, Event } from "../types/event";
 
-export function createEvent<T extends ClientEventKey, Ready extends boolean = true>(event: Event<T, Ready>) {
+export function createEvent<
+  T extends ClientEventKey,
+  Ready extends boolean = true,
+>(event: Event<T, Ready>) {
   return event;
 }
 
-export function createCommand(command: AnyCommand) {
+export function createCommand<T extends AnyCommand>(command: T): T {
   return command;
 }
